9b2e018bec90fc74c1bbba55775f9f769aa8dda5
Carlossnogueira <carlosnogueira.tkdev@gmail.com>
2025-04-05 15:25:42 -0300

    metodo get criado

--- app.py
+++ app.py
@@ -1,17 +1,24 @@
-from flask import Flask, request
-from models.Task import task
+from flask import Flask, request, jsonify
+from models.Task import Task
 
 # __name__ - "__main__"
 app = Flask(__name__)
 
 tasks = []
+task_id_control = 1
 
 # CRUD
 @app.route('/tasks', methods=['POST'])
 def create_task():
-    # recepção de dados:
-    data = request.get_json()
-    return 'Test'
+   global task_id_control #usando variaveis de fora na função
+   #captura a entrada do json
+   data = request.get_json()
+   new_task = Task(id=task_id_control, title=data['title'], description=data.get("description", ""))
+   task_id_control += 1
+   tasks.append(new_task)
+   print(tasks)
+   #retorno em json
+   return jsonify({"message": "Nova tarefa criada com sucesso"})
 
     
 # Logs de consulta debug=true
--- models/Task.py
+++ models/Task.py
@@ -1,14 +1,14 @@
-class Tasl:
-    def __init__(self,id,title,description,completed=False) -> None:
-        self.id = id
-        self.tittle = title
-        self.description = description
-        self.completed = completed
-        
-    def to_dict(self):
-        return{
-            "id": self.id,
-            "title": self.tittle,
-            "description": self.description,
-            "completed": self.completed
-        }
+class Task:
+   def __init__(self, id, title, description, completed=False) -> None:
+     self.id = id
+     self.title = title
+     self.description = description
+     self.completed = completed
+ 
+   def to_dict(self):
+     return {
+       "id": self.id,
+       "title": self.title,
+       "description": self.description,
+       "completed": self.completed
+     }
