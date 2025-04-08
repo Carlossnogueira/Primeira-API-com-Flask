e58f17735b72443671566cc99d6e882be88e20cb
Carlossnogueira <carlosnogueira.tkdev@gmail.com>
2025-04-08 20:35:04 -0300

    testes e correção no código principal

--- app.py
+++ app.py
@@ -1,79 +1,71 @@
 from flask import Flask, request, jsonify
-from models.Task import Task
-
-# __name__ - "__main__"
+from models.task import Task
 app = Flask(__name__)
+
+# CRUD
+# Create, Read, Update and Delete = Criar, Ler, Atualizar, Deletar
+# Tabela: Tarefa
 
 tasks = []
 task_id_control = 1
 
-# CRUD
-
-#Create
 @app.route('/tasks', methods=['POST'])
 def create_task():
-   global task_id_control #usando variaveis de fora na função
-   #captura a entrada do json
-   data = request.get_json()
-   new_task = Task(id=task_id_control, title=data['title'], description=data.get("description", ""))
-   task_id_control += 1
-   tasks.append(new_task)
-   print(tasks)
-   #retorno em json
-   return jsonify({"message": "Nova tarefa criada com sucesso"})
+  global task_id_control
+  data = request.get_json()
+  new_task = Task(id=task_id_control, title=data['title'], description=data.get("description", ""))
+  task_id_control += 1
+  tasks.append(new_task)
+  print(tasks)
+  return jsonify({"message": "Nova tarefa criada com sucesso", "id": new_task.id})
 
-#Read All
 @app.route('/tasks', methods=['GET'])
 def get_tasks():
-   #for rodando dentro da lista
-   task_list = [task.to_dict() for task in tasks]
-   output = {
-      "tasks": task_list,
-      "total_tasks": len(task_list)
-   }
-   return jsonify(output)
+  task_list = [task.to_dict() for task in tasks]
 
-#Read by Id
-@app.route('/tasks/<int: id>', methods=['GET'])
+  output = {
+    "tasks": task_list,
+    "total_tasks": len(task_list)
+  }
+  return jsonify(output)
+
+@app.route('/tasks/<int:id>', methods=['GET'])
 def get_task(id):
-   task = None
-   for t in tasks:
-      if t.id == id:
-         return jsonify(t.to_dict())
-      
-   return jsonify({"message":"Não foi possível encontrar a atividade"}), 404
+  for t in tasks:
+    if t.id == id:
+      return jsonify(t.to_dict())
+  return jsonify({"message": "Não foi possível encontrar a atividade"}), 404
 
-#Update
 @app.route('/tasks/<int:id>', methods=["PUT"])
 def update_task(id):
-   task = None
-   for t in tasks:
-     if t.id == id:
-       task = t
-   if task == None:
-     return jsonify({"message": "Não foi possível encontrar a atividade"}), 404
- 
-   data = request.get_json()
-   task.title = data['title']
-   task.description = data['description']
-   task.completed = data['completed']
-   return jsonify({"message": "Tarefa atualizada com sucesso"})
+  task = None
+  for t in tasks:
+    if t.id == id:
+      task = t
+      break
+    
+  if task == None:
+    return jsonify({"message": "Não foi possível encontrar a atividade"}), 404
 
-#Delete
+  data = request.get_json()
+  task.title = data['title']
+  task.description = data['description']
+  task.completed = data['completed']
+  return jsonify({"message": "Tarefa atualizada com sucesso"})
+
 @app.route('/tasks/<int:id>', methods=['DELETE'])
 def delete_task(id):
-   task = None
-   for t in tasks:
-     if t.id == id:
-       task = t
-       break
- 
-     if not task:
-       return jsonify({"message": "Não foi possível encontrar a atividade"}), 404
-     
-     tasks.remove(task)
-     return jsonify({"message": "Tarefa deletada com sucesso"})
+  task = None
+  for t in tasks:
+    if t.id == id:
+      task = t
+      break
+
+    if not task:
+      return jsonify({"message": "Não foi possível encontrar a atividade"}), 404
     
-# Logs de consulta debug=true
+    tasks.remove(task)
+    return jsonify({"message": "Tarefa deletada com sucesso"})
+
 if __name__ == "__main__":
-    app.run(debug=True)
+  app.run(debug=True)
